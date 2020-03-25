e0238d43b170ef56b3e9440c2bebdc319f585fdd
Rafael Campos <rafaelccamposdev@gmail.com>
2020-03-25 14:15:25 -0300

    adicionando express.json para conversão o reqes.body, instalação do nodemon e criação do script start

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/',(Request, Response) => {
     return Response.json({
         evento: 'Semana OminiStack 11.0',

756c5b366e249b5f3495db5ca81fc94bbd412c0f
Rafael Campos <rafaelccamposdev@gmail.com>
2020-03-26 12:24:43 -0300

    criação do connection para a cfg de acesso ao banco, criação dos controllers Incident, Ong, Profile e Sessios centralizando as functions neles deixando somente a chamada delas em routes. Instalação do cors para segurança da aplicação.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors({
+    //restringindo o acesso da aplicação para o domínio utilizado
+    //origin: 'http://meuapp.com'
+}
+));
 app.use(express.json());
 app.use(routes);
 

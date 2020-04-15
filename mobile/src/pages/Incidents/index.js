6fd3b5f5d1ccebe741c45084f3d29fda22bccad4
unknown <rafaelccamposdev@gmail.com>
2020-04-14 22:57:39 -0300

    Instalação do axios, integrando tela de incidents ao backend, com o sistema de paginação.

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Image, FlatList, Text, TouchableOpacity } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+
+import api from '../../services/api';
+
 import logoImg from '../../assets/logo.png';
 
 import styles from './styles';
 
 export default function Incidents() {
+    const [incidents, setIncidents] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+
     const navigation = useNavigation();
 
     function navigateToDetail() {
         navigation.navigate('Detail');
     }
+
+    async function loadIncidents () {
+
+        if (loading) {
+            return;
+        }
+        
+        setLoading(true);
+
+        if (total > 0 && incidents.length === total) {
+            return;
+        }
+
+
+        const response = await api.get('incidents',{
+            params: {page}
+        }); 
+
+        setIncidents([...incidents, ...response.data]);
+        setTotal(response.headers['x-total-count']);
+        setPage(page + 1);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        loadIncidents();
+    }, []);
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg} />
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+                    Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
                 </Text>
             </View>
 
@@ -26,19 +62,20 @@ export default function Incidents() {
 
             <FlatList
                 style={styles.incidentList}
-                data= {[1, 2, 3, 4, 5]}
-                keyExtractor={incident => String(incident)}
-                showsVerticalScrollIndicator={false}
-                renderItem={() => (
+                data= {incidents}
+                keyExtractor={incident => String(incident.id)}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.3}
+                renderItem={({ item: incident}) => (
                     <View style={styles.incident}> 
                         <Text style={styles.incidentProperty}>ONG:</Text>
-                        <Text style={styles.incidentValue}>APA</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
 
                         <Text style={styles.incidentProperty}>CASO:</Text>
-                        <Text style={styles.incidentValue}>Cachorrinho atropelado.</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
 
                         <Text style={styles.incidentProperty}>VALOR:</Text>
-                        <Text style={styles.incidentValue}>R$ 150,00</Text>
+                        <Text style={styles.incidentValue}>{incident.value}</Text>
                         
                         <TouchableOpacity
                             style={styles.detailsButton}
